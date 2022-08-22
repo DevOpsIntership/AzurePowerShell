c725d659d710f721a4341441966289e89fe24bd8
adkwolek <adam.kwolek@capgemini.com>
2022-08-22 21:55:42 +0200

    Vnet

---
+++ vnet.ps1
@@ -0,0 +1,14 @@
+Write-Host 'Create Vnet';
+$name='Vnet';
+$location='northeurope';
+$rg='psautomation'
+$address='10.0.0.0/16'
+$arraysubnet = '10.0.0.0/25', '10.0.0.128/25';
+$subnetName  = 'ubuntu','windows';
+
+$VirtualNetwork = New-AzVirtualNetwork -Name $name -Location $location -ResourceGroupName $rg -AddressPrefix $address
+for ($i = 0; $i -lt $arraysubnet.Count; $i++) {
+  Add-AzVirtualNetworkSubnetConfig -Name $subnetName[$i] -VirtualNetwork $VirtualNetwork -AddressPrefix $arraysubnet[$i] 
+}
+$virtualNetwork | Set-AzVirtualNetwork
+
